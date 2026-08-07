0497a6c5afe2d0958846d372ed4a8889d0dcce9d
Andrew Davis <andrew.davis@ukaea.uk>
2026-08-07 20:04:33 +0100

    Incorrect disallowing in randomray mode

--- src/tallies/StructuredMeshTally.C
+++ src/tallies/StructuredMeshTally.C
@@ -71,11 +71,6 @@ StructuredMeshTally::StructuredMeshTally(const InputParameters & parameters)
     _mesh_type(getParam<MooseEnum>("mesh_type") == "rectilinear" ? structured_mesh::MeshType::RECTILINEAR
                                                                  : structured_mesh::MeshType::REGULAR)
 {
-  // Structured meshes are incompatible with the random ray solver; that solver uses
-  // tracklength estimators which are not defined for the structured spatial filters treated here.
-  if (_openmc_problem.runRandomRay())
-    mooseError("Structured mesh tallies are not supported when using the random ray solver!");
-
   // The structured mesh is fully independent of the [Mesh], so we reuse it as-is.
   _mesh = std::make_unique<structured_mesh::StructuredMesh>(
       buildCoordinates(), _dimension, _mesh_type);
